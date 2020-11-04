66477ea35a4dd099bf74433386b2167859621ce6
josebira <73902264+josebira@users.noreply.github.com>
2020-11-04 10:40:50 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const URL="https://cpf-spring-boot.herokuapp.com/"
+const URL="https://cpf-spring-boot.herokuapp.com/validacpf?cpf="
 function validar(){
 
 	var txtCPF = document.getElementById("txtCpf").value;
